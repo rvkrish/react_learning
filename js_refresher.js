bba3d84efb7e2ae76cc7e994692de50bc687fe7e
rvkrish <tech.rvkrish@gmail.com>
2023-01-11 17:57:28 +0000

    all changes

--- js_refresher.js
+++ js_refresher.js
@@ -1,5 +1,95 @@
-test = ()=> {
-    console.log("hello")
+/*
+Understand var vs let vs const
+/*
+
+
+
+
+//------------------------------------------------------------------------------------------------------------ 
+
+
+
+Arrow Functions:
+
+The below two functions are same.
+
+*/
+
+function testFunctionOne(){
+    console.log("hello from test 1")
+}
+
+const testFunctionTwo = ()=> {
+    console.log("hello from test 2")
 } 
 
-test();
+testFunctionOne();
+testFunctionTwo();
+
+
+//The below two functions are same. If we have only one parameter for arrow functons small paranthisis is optional
+
+function printMyNameOne(name){
+    console.log(name+"from printMyNameOne");
+}
+
+const printMyNameTwo = (name) =>  {
+    console.log(name+"from printMyNameTwo");
+}
+
+const printMyNameThree = name =>  {
+    console.log(name+"from printMyNameThree");
+}
+
+printMyNameOne("My Name");
+printMyNameTwo("My Name");
+printMyNameThree("My Name");
+
+//The below two functions are same.
+
+function printMyNameAgeOne(name,age){
+    console.log(name+"from printMyNameOne");
+    console.log(age+"from printMyNameOne");
+}
+
+const printMyNameAgeTwo = (name,age) =>  {
+    console.log(name+"from printMyNameTwo");
+    console.log(age+"from printMyNameTwo");
+}
+
+printMyNameAgeOne("My Name",20);
+printMyNameAgeTwo("My Name",21);
+
+
+//How the arguments are returned, Below three functions are same. if we omit flower brackets then we can write a single line of code which will be returned 
+
+function multiplyOne(num1,num2){
+   return num1*num2;
+}
+
+const multiplyTwo = (num1,num2) =>  {
+    return num1*num2;
+}
+
+const multiplyThree = (num1,num2) =>  num1*num2;
+
+
+console.log(multiplyOne(2,4))
+console.log(multiplyTwo(2,4))
+console.log(multiplyThree(2,4))
+
+// No More issues with ** this ** keyword if we use arrow function.
+
+
+//------------------------------------------------------------------------------------------------------------ 
+
+
+/* Imports and exports using JS */
+
+
+
+
+
+
+
+
